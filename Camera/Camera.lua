031744537e7ee3b83bb913fc489944d176b2377a
MikeXander <40724902+MikeXander@users.noreply.github.com>
2020-07-28 18:30:41 -0400

    Add J Support

--- Camera/Camera.lua
+++ Camera/Camera.lua
@@ -1,29 +1,68 @@
 --[[
     Handle Camera in RAM
-    Special Thanks: MKDasher
+
+    Special Thanks:
+		MKDasher for original code
+		pfedak for proper pointer paths
 ]]
 
 local Cam = {}
 
--- to be able to change the position this needs to be called with pos=0
-function Cam.ApplyCameraHack(pos, angle)
-	if pos ~= nil then memory.writedword(0x00A87cf0, pos) end
-	if angle ~= nil then memory.writedword(0x00A87d08, angle) end
-	memory.recompilenextall()
+-- detect the ROM for appropriate pointers
+local ROM_list = {
+	addr = 0x802F0000,
+	U = 0xC58400A4,
+	J = 0x27BD0020
+}
+
+local ROM = memory.readdword(ROM_list.addr)
+
+if ROM == ROM_list.U then
+	ROM = "U"
+	print("U ROM detected")
+elseif ROM == ROM_list.J then
+	ROM = "J"
+	print("J ROM detected")
+else
+	ROM = nil
+	print("Error: ROM must be U or J")
 end
 
+
 local function WriteRenderCamera(camstruct)
-	local pointeraddr = 0x00A06BDC
-	local addr = memory.readdword(pointeraddr)
-	if addr > 0x80000000 and addr < 0x80300000 then
-		addr = addr - 0x80000000 + 0x80001C
-		if camstruct.x ~= nil then memory.writefloat(addr, camstruct.x) end
-		if camstruct.y ~= nil then memory.writefloat(addr + 4, camstruct.y) end
-		if camstruct.z ~= nil then memory.writefloat(addr + 8, camstruct.z) end
-		if camstruct.xfocus ~= nil then memory.writefloat(addr + 12, camstruct.xfocus) end
-		if camstruct.yfocus ~= nil then memory.writefloat(addr + 16, camstruct.yfocus) end
-		if camstruct.zfocus ~= nil then memory.writefloat(addr + 20, camstruct.zfocus) end
-	end
+	if not ROM then return end
+
+	local offset = 0x00800000 - 0x80000000
+	gCurrentArea ={
+		U = 0x8032ddcc,
+		J = 0x8032ce6c
+	}
+	area = memory.readdword(gCurrentArea[ROM] + offset) + offset
+	root = memory.readdword(area + 0x04) + offset
+	addr = memory.readdword(root + 0x24) + offset + 0x1c
+
+	if camstruct.x ~= nil then memory.writefloat(addr, camstruct.x) end
+	if camstruct.y ~= nil then memory.writefloat(addr + 4, camstruct.y) end
+	if camstruct.z ~= nil then memory.writefloat(addr + 8, camstruct.z) end
+	if camstruct.xfocus ~= nil then memory.writefloat(addr + 12, camstruct.xfocus) end
+	if camstruct.yfocus ~= nil then memory.writefloat(addr + 16, camstruct.yfocus) end
+	if camstruct.zfocus ~= nil then memory.writefloat(addr + 20, camstruct.zfocus) end
+end
+
+-- Passing 0 as an argument prevents the camera from updating
+-- Which allows us to manually edit the cam position
+function Cam.ApplyCameraHack(pos, focus)
+	if not ROM then return end
+
+	Create_Camera = {
+		U = 0x287be0,
+		J = 0x2875f8,
+	}
+	local offset = 0x800110
+
+	if pos ~= nil then memory.writedword(Create_Camera[ROM] + offset, pos) end
+	if focus ~= nil then memory.writedword(Create_Camera[ROM] + offset + 0x18, focus) end
+	memory.recompilenextall()
 end
 
 function Cam.SetCamPos(pos)
